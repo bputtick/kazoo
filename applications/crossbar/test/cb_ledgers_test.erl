722109a90515cf7bcd9c17e0c885460e0a6a9ea9
James Aimonetti <james.aimonetti@gmail.com>
2019-01-23 23:35:56 +0000

    start matching cell counts

--- applications/crossbar/test/cb_ledgers_test.erl
+++ applications/crossbar/test/cb_ledgers_test.erl
@@ -14,17 +14,36 @@ to_csv_test_() ->
     NoDesc = load(TestDir, "ledger_no_description.json"),
     WithDesc = load(TestDir, "ledger_description.json"),
 
-    NoDescCSV = api_util:csv_body(NoDesc, 'true'),
+    NoDescCSV = iolist_to_binary(api_util:csv_body([NoDesc], 'true')),
 
-    WithDescCSV = api_util:csv_body(WithDesc, 'false'),
+    [HeadersRow, NoDescRow | _] = binary:split(NoDescCSV, <<"\n">>, ['global']),
 
-    ?debugFmt("~nno: ~p~n~s~n~nwi: ~p~n~s~n~n"
-             ,[NoDescCSV, NoDescCSV
-              ,WithDescCSV, WithDescCSV
-              ]),
-    ?_assert('false').
+    WithDescRow = iolist_to_binary(api_util:csv_body([WithDesc], 'false')),
 
+    BothCSV = iolist_to_binary(api_util:csv_body([NoDesc, WithDesc], 'true')),
+
+    ?debugFmt("~s~n~s~n~s~n~n", [HeadersRow, NoDescRow, WithDescRow]),
+
+    ?debugFmt("~s~n~n", [BothCSV]),
+
+    Ls = [{h, cell_count(HeadersRow)}, {n, cell_count(NoDescRow)}, {w, cell_count(WithDescRow)}],
+
+    ?debugFmt("lengths: ~p~n", [Ls]),
+
+    ?_assert(all_match(Ls)).
+
+cell_count(Row) ->
+    length(binary:split(Row, <<"\",\"">>, ['global'])).
+
+all_match([{_, A}, {D, A} | T]) ->
+    all_match([{D, A} | T]);
+all_match([_]) -> 'true';
+all_match([]) ->  'true';
+all_match(_F) ->
+    ?debugFmt("not matching ~p", [_F]),
+    'false'.
 
 load(Dir, File) ->
     {'ok', JSON} = file:read_file(filename:join([Dir, File])),
-    kz_json:decode(JSON).
+    JObj = kz_json:decode(JSON),
+    kz_ledger:public_json(kz_ledger:from_json(JObj)).
